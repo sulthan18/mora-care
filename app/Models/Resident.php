3f88456e1ada3466981be9c00362e815978d4b3c
sulthan rafi hendriansyah <sulthan19@gmail.com>
2025-05-08 21:05:26 +0700

    feat: "add model and migration resident"

---
+++ app/Models/Resident.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Resident extends Model
+{
+    protected $fillable = [
+      'user_id',
+      'avatar'  
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class);
+    }
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -45,4 +45,8 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+
+    public function Resident(){
+        return $this->hasOne(Resident::class);
+    }
 }
