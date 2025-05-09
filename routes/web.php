cb916f17ba2ce5d59b54edc002fc83f9e43427da
sulthan rafi hendriansyah <sulthan19@gmail.com>
2025-05-09 10:21:06 +0700

    feat: "update layout admin page"

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->name('admin.')->group(function () {
+    Route::get('/dashboard', function () {
+        return view('pages.admin.dashboard');
+    })->name('dashboard');
+});
