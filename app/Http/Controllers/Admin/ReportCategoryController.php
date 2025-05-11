523195192d086e3e67ac83b9c7399fe8284197a3
sulthan rafi hendriansyah <sulthan19@gmail.com>
2025-05-11 13:29:46 +0700

    feat: "edit data for admin data report category"

--- app/Http/Controllers/Admin/ReportCategoryController.php
+++ app/Http/Controllers/Admin/ReportCategoryController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreReportCategoryRequest;
+use App\Http\Requests\UpdateReportCategoryRequest;
 use App\Interfaces\ReportCategoryRepositoryInterface;
 use Illuminate\Http\Request;
+use RealRashid\SweetAlert\Facades\Alert as Swal;
 
 class ReportCategoryController extends Controller
 {
@@ -29,15 +32,23 @@ class ReportCategoryController extends Controller
      */
     public function create()
     {
-        //
+        return view('pages.admin.category.create');
     }
 
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreReportCategoryRequest $request)
     {
-        //
+        $data = $request->validated();
+
+        $data['image'] = $request->file('image')->store('assets/report-category/image', 'public');
+
+        $this->reportCategoryRepository->createReportCategory($data);
+
+        Swal::toast('Data Kategori Berhasil Ditambahkan', 'success')->timerProgressBar();
+
+        return redirect()->route('admin.report-category.index');
     }
 
     /**
@@ -53,15 +64,27 @@ class ReportCategoryController extends Controller
      */
     public function edit(string $id)
     {
-        //
+        $category = $this->reportCategoryRepository->getReportCategoryById($id);
+
+        return view('pages.admin.category.edit', compact('category'));
     }
 
     /**
      * Update the specified resource in storage.
      */
-    public function update(Request $request, string $id)
+    public function update(UpdateReportCategoryRequest $request, string $id)
     {
-        //
+        $data = $request->validated();
+
+        if ($request->image) {
+            $data['image'] = $request->file('image')->store('assets/report-category/image', 'public');
+        }
+
+        $this->reportCategoryRepository->updateReportCategory($data, $id);
+
+        Swal::toast('Data Kategori Berhasil Diupdate', 'success')->timerProgressBar();
+
+        return redirect()->route('admin.report-category.index');
     }
 
     /**
