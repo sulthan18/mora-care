c583396f6c0af2051edbd88fa8c37426ee83a331
sulthan rafi hendriansyah <sulthan19@gmail.com>
2025-05-20 11:34:37 +0700

    feat: "add auth name for user"

--- resources/views/pages/app/home.blade.php
+++ resources/views/pages/app/home.blade.php
@@ -3,7 +3,7 @@
 @section('title', 'Home')
 
 @section('content')
-    <h6 class="greeting">Hi, User 👋</h6>
+    <h6 class="greeting">Hi, {{Auth::user()->email}}👋</h6>
     <h4 class="home-headline">Laporkan masalahmu dan kami segera atasi itu</h4>
 
     <div class="d-flex align-items-center justify-content-between gap-4 py-3 overflow-auto" id="category" style="white-space: nowrap;">
